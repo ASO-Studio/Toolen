665cee2bfd0cd4e82d6719be73238be53f6a5f1a
RoofAlan <m15024617172@163.com>
2025-08-09 12:51:38 +0800

    Added functions: mkfifo

---
+++ file/mkfifo.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include "config.h"
+#include "module.h"
+
+// Show help
+static void mkfifo_show_help() {
+	SHOW_VERSION(stderr);
+	fprintf(stderr, "Usage: mkfifo [NAME]...\n\n"
+			"Create FIFOs (named pipes)\n");
+}
+
+// Main functions
+int mkfifo_main(int argc, char *argv[]) {
+	if(!argv[1]) {
+		fprintf(stderr, "mkfifo: no argument gave\n"
+				"Try pass '--help' for more details\n");
+	} else if (strcmp(argv[1], "--help") == 0) {
+		mkfifo_show_help();
+		return 0;
+	}
+
+	// Default mode: 0666 (-rw-rw-rw-), real: prw-rw-r--
+	mode_t mode = 0666;
+	for(int i = 1; i < argc; i++) {
+		if (mkfifo(argv[i], mode) < 0) {
+			perror("mkfifo");	// Print error informations when failed to create pipe
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+REGISTER_MODULE(mkfifo);
