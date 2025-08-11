cd7ff6ff54a2788d24a0005e1287c3166bfd402b
RoofAlan <m15024617172@163.com>
2025-08-11 08:41:25 +0800

    Added functions: pwd

---
+++ basic/pwd.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "config.h"
+#include "module.h"
+
+static void pwd_show_help() {
+	SHOW_VERSION(stderr);
+	fprintf(stderr, "Usage: pwd\n\n"
+			"Print working(current) directory\n\n"
+			"Support options:\n"
+			"  -L   Print from environment($PWD)\n");
+}
+
+int pwd_main(int argc, char *argv[]) {
+	int opt;
+	int opt_index = 0;
+	struct option opts[] = {
+		{"L", no_argument, 0, 'L'},
+		{"help", no_argument, 0, 'h'},
+		{"version", no_argument, 0, 'V'}
+	};
+
+	int useEnv = 0;
+
+	// Parse arguments
+	while((opt = getopt_long(argc, argv, "LhV", opts, &opt_index)) != -1) {
+		switch(opt) {
+			case 'L':
+				useEnv = 1;
+				break;
+			case 'h':
+				pwd_show_help();
+				return 0;
+			case 'V':
+				JUST_VERSION();
+				return 0;
+			case '?':
+				fprintf(stderr, "Try pass '--help' for more details\n");
+				return 1;
+		}
+	}
+
+	// Print working directory from environment
+	if (useEnv) {
+		printf("%s\n", getenv("PWD") ? getenv("PWD") : "");
+		return 0;
+	}
+
+	// Print from 'getcwd()'
+	char workDir[512];
+	getcwd(workDir, sizeof(workDir));
+	printf("%s\n", workDir);
+	return 0;
+}
+
+REGISTER_MODULE(pwd);
