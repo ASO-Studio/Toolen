205f0955125b6b8d19ef1b27b5183633161af444
RoofAlan <m15024617172@163.com>
2025-08-23 19:36:03 +0800

    Added functions: dirname basename

---
+++ basic/dirname.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <libgen.h>
+
+#include "config.h"
+#include "module.h"
+#include "lib.h"
+
+static void dirname_show_help() {
+	SHOW_VERSION(stderr);
+	fprintf(stderr, "Usage: dirname PATH...\n\n"
+			"Show directory portion of path\n");
+}
+
+int dirname_main(int argc, char *argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "dirname: Need 1 argument\n"
+				"Try pass '--help' for more details\n");
+		return 1;
+	}
+
+	if (findArg(argv, argc, "--help")) {
+		dirname_show_help();
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		if (argv[i] && argv[i][0] != '-') {
+			printf("%s\n", dirname(argv[i]));
+		}
+	}
+
+	return 0;
+}
+
+REGISTER_MODULE(dirname);
