d4738a1cc813afd6c4ed0b0864d0d4defa5be2e0
RoofAlan <m15024617172@163.com>
2025-08-30 20:32:58 +0800

    Added function: link

---
+++ file/link.c
@@ -0,0 +1,45 @@
+/**
+ *	link.c - Create a hardlink
+ *
+ * 	Created by RoofAlan
+ *		2025/8/30
+ *
+ *	Copyright (C) 2025 ASO-Studio
+ *	Based on MIT protocol open source
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "config.h"
+#include "module.h"
+#include "lib.h"
+
+static void link_show_help() {
+	SHOW_VERSION(stderr);
+	fprintf(stderr, "Usage: link FILE NEWFILE\n\n"
+			"Create a hardlink\n");
+}
+
+int link_main(int argc, char *argv[]) {
+	if (argc < 3) {
+		pplog(P_NAME | P_HELP, "Need 2 arguments");
+		return 1;
+	} else if (argc > 3) {
+		pplog(P_NAME | P_HELP, "Max 2 arguments");
+		return 1;
+	}
+
+	if (argc > 1 && findArg(argv, argc, "--help")) {
+		link_show_help();
+		return 0;
+	}
+
+	if (link(argv[1], argv[2]) < 0) {
+		perror("link");
+		return 1;
+	}
+	return 0;
+}
+
+REGISTER_MODULE(link);
